bef188859b2d74ff8e475528542294f36fa7bd3d
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-20 11:45:49 -0500

    Update phasetwo.js

--- phasetwo.js
+++ phasetwo.js
@@ -1,124 +1,90 @@
+
+// In this version data are handled as STRINGS and string operations are used.
 const express = require('express');
 const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
 var fs = require("fs");
+const { clear } = require('console');
 
 app.listen(port);
-console.log('Server started at http://localhost:' + port);
+console.log('Server started at port:' + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// GET All tickets
+// ---------------------
+// Routes for the app
+// ---------------------
 
-app.get("/rest/list/", function(req, res) {
-    fs.readFile("tickets.txt", (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT") { 
-                //Error No Entry
-                //if file doesn't exist we throw an error 
-                res.status(404).send("File with Tickets does not exist!");
-            } else {
-                console.error(err);
-                res.status(500).send("Server error!");
-            }
-            return;
-        } else {
-            console.log("GET all tickets was successful!\n");
-        }
-
-        //creating javascript object - array tickets
-        const tickets = JSON.parse(data.toString('utf8'));
-        //sending response - all tickets
-        res.json(tickets);
-      });
+app.get('/', function(req, res) {
+  const myquery = req.query;
+  var outstring = 'Started and listening... ';
+  res.send(outstring);
 });
 
-// GET ticket by id
 
-app.get('/rest/ticket/:id', function(req, res) {
-    //JSON.parse treats id as a number thus we have to treat it as a number in input
-    const inputId = Number(req.params.id);
-    console.log("Looking for: " + inputId);
-
-    fs.readFile("tickets.txt", (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT") {
-                //ENOENT = Error No Entry
-                //if file doesn't exist we throw an error 
-                res.status(404).send("File with Tickets does not exist!");
-            } else {
-                console.error(err);
-                res.status(500).send("Server error!");
-            }
-            return;
-        } else {
-            console.log("GET all tickets was successful!\n");
-        }
-
-        const tickets = JSON.parse(data.toString('utf8'));
-        //we search for ticket who's id matches the inputId
-        const ticket = tickets.find((ticket) => ticket.id === inputId);
-
-        //if ticket is not found it will be undefined
-        if (!ticket) {
-            res.status(404).send("Ticket does not exist!");
-        } else {
-            console.log("Ticket exists!")
-            //sending response - ticket
-            res.json(ticket);
-        }
-    });
-});
-
-// A POST request
-
-app.post('/rest/ticket/', function(req, res) {
-    const newTicket = req.body;
-
-    //fields needed in the body
-    const ticketInfo = ['id', 'created_at', 'updated_at', 'type', 'subject', 'description', 'priority', 
-                        'status', 'recipient', 'submitter', 'assignee_id', 'follower_ids', 'tags'];
-    //checking how many fields are missing
-    const missingTicketInfo = ticketInfo.filter(field => !(field in newTicket));
-
-    //if more than 0 are missing then throw an error
-    if (missingTicketInfo.length > 0) {
-        return res.status(400).json({ 
-            error: `Incomplete ticket info!\n Missing fields: ${missingTicketInfo.join(', ')}`});
+// Show the form for POSTing input
+app.get('/form', function(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./post.html', 'utf8', (err, contents) => {
+    if(err) {
+        console.log('Form file Read Error', err);
+        res.write("<p>Form file Read Error");
+    } else {
+        console.log('Form loaded\n');
+        res.write(contents + "<br>");
     }
+    res.end();
+  });
+});
 
-    //adding the new ticket
-    fs.readFile("tickets.txt", (err, data) => {
-        if (err) {
-            if (err.code === "ENOENT") { //Error No Entry
-                //if file doesn't exist we throw an error 
-                res.status(404).send("File with Tickets does not exist!");
-            } else {
-                console.error(err);
-                res.status(500).send("Server error!");
-            }
-            return;
-        } else {
-            console.log("GET all tickets was successful!\n");
-        }
 
-        //creating javascript object - array tickets
-        const tickets = JSON.parse(data.toString('utf8'));
+// List all tickets (which are stored in a file)
+app.get('/rest/list', function(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./mydata_str.txt', 'utf8', (err, data) => {
+    if(err) {
+        console.log('File Read Error', err);
+        res.write("<p>File Read Error");
+    } else {
+        console.log('Tickets loaded\n');
+        res.write(data.replace('\n','<br>') + "<br>Listing completed");
+    }
+    res.end();
+  });
+});
 
-        //Adding the new ticket to the array
-        tickets.push(newTicket)
+// Search for a specific ticket (id)
+// The file assumed to contain records as strings, one record per line.
+// Here we use a regular expression match to find the record of interest.
+app.get('/rest/ticket/:id', function(req, res) {
+  const regexString = new RegExp('id":"' + req.params.id + '.*');
+  console.log("Request for ID: " + req.params.id);
+  console.log("RegExp: " + regexString + "\n");
 
-        //Saving the ticket in the file
-        fs.writeFile("tickets.txt", JSON.stringify(tickets, null, 2), (err) => {
-            if (err) {
-            return res.status(500).json({ 
-                error: "Error writing to file!"});
-            } else {
-                console.log("Ticket added!")
-                res.status(201).json(newTicket);
-            }
-        });
-    });
+  var allrecords = fs.readFile("./mydata_str.txt", 'utf8', function(err, doc) {
+    var result = doc.match(regexString);
+    console.log("Result: " + result + "Done\n");
+    res.send(result);
+  });
+});
+
+
+
+// A POST request to insert a record by appending it as a string in a new line
+app.post('/rest/maketicket', function(req, res) {
+  const body = req.body;
+  // Report to console what was received (for debugging)
+  console.log(req.body);
+  var inp_string = JSON.stringify(req.body);
+  fs.appendFile("mydata_str.txt", inp_string +'\n', (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("Wrote inp_string to file");
+    }
+  // Report to the user
+  res.send(inp_string + " <p>stored to file");
+  });
 });
