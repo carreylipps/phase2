fdf0bde6dd1373273d9213ee5b1ba6d5338c77c9
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-20 17:01:26 -0500

    Update phasetwo.js

--- phasetwo.js
+++ phasetwo.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 var fs = require("fs");
 const { clear } = require('console');
+const { MongoClient } = require('mongodb');
 
 app.listen(port);
 console.log('Server started at port:' + port);
@@ -13,6 +14,17 @@ console.log('Server started at port:' + port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//connect Mongodb
+const uri = 'mongodb+srv://carrey:beauxbella@clmdb.xd8c4zo.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopolgy: true});
+client.connect((err) => {
+  if (err) {
+    console.log('MongoDB error:', err);
+    process.exit(1);
+  } else {
+    console.log('MongoDB connected');
+  }
+});
 // ---------------------
 // Routes for the app
 // ---------------------
@@ -42,16 +54,15 @@ app.get('/form', function(req, res) {
 
 // List all tickets (which are stored in a file)
 app.get('/rest/list', function(req, res) {
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('./tickets.txt', 'utf8', (err, data) => {
+  const collection = client.db('clmdb').collection('ticket');
+  collection.find().toArray((err, result) => {
     if(err) {
-        console.log('File Read Error', err);
-        res.write("<p>File Read Error");
+        console.log('MongoDB Error', err);
+        res.write('Error' + err);
     } else {
         console.log('Tickets loaded\n');
-        res.write(data.replace('\n','<br>') + "<br>Listing completed");
+        res.send(result);
     }
-    res.end();
   });
 });
 
@@ -59,14 +70,20 @@ app.get('/rest/list', function(req, res) {
 // The file assumed to contain records as strings, one record per line.
 // Here we use a regular expression match to find the record of interest.
 app.get('/rest/ticket/:id', function(req, res) {
-  const regexString = new RegExp('id":"' + req.params.id + '.*');
+  const collection = client.db('ticketsdb').collection('tickets');
+  collection.findOne({ id: req.params.id }, (err, result) => {
+  
+   if (err) {
+     console.log('MongoDB error:', err);
+     res.send('Error: ' + err);
+   } else if (result) {
   console.log("Request for ID: " + req.params.id);
-  console.log("RegExp: " + regexString + "\n");
-
-  var allrecords = fs.readFile("./tickets.txt", 'utf8', function(err, doc) {
-    var result = doc.match(regexString);
-    console.log("Result: " + result + "Done\n");
-    res.send(result);
+  console.log("Result: " + result + "\n");
+  res.send(result);
+   } else {
+     console.log("Ticket not here");
+     res.send("Ticket not here");
+   }
   });
 });
 
@@ -74,17 +91,16 @@ app.get('/rest/ticket/:id', function(req, res) {
 
 // A POST request to insert a record by appending it as a string in a new line
 app.post('/rest/maketicket', function(req, res) {
-  const body = req.body;
-  // Report to console what was received (for debugging)
-  console.log(req.body);
-  var inp_string = JSON.stringify(req.body);
-  fs.appendFile("tickets.txt", inp_string +'\n', (err) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("Wrote inp_string to file");
+  const collection = client.db('clmdb').collection('ticket');
+  const ticket = req.body;
+  collection.insertOne(ticket, (err, result) => {
+    if (err) {
+      console.log('MongoDB error:', err);
+      res.send('ErrorL ' + err);
+    } else {
+      console.log("Ticket inserted");
+      res.send(ticket + "<p>in database");
     }
-  // Report to the user
-  res.send(inp_string + " <p>stored to file");
+  
   });
 });
