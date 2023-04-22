5a675531cfad5218fa02301860db660757882b50
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-21 20:31:49 -0500

    Update phasetwo.js

--- phasetwo.js
+++ phasetwo.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
-const fs = require("fs");
 const { MongoClient } = require('mongodb');
 const { ObjectId } = require('mongodb');
 
@@ -54,32 +53,29 @@ app.get('/rest/list', function(req, res) {
   collection.find().toArray((err, result) => {
     if(err) {
         console.log('MongoDB Error', err);
-        res.write('Error' + err);
+        res.status(500).send('Error');
     } else {
         console.log('Tickets loaded\n');
         res.send(result);
     }
-    res.end();
   });
 });
 
 // Search for a specific ticket id
 app.get('/rest/ticket/:id', function(req, res) {
-  const collection = client.db('ticketsdb').collection('tickets');
-  collection.findOne({ _id: req.params.id }, (err, result) => {
+  collection.findOne({ _id: ObjectId(req.params.id) }, (err, result) => {
   
    if (err) {
      console.log('MongoDB error:', err);
-     res.send('Error: ' + err);
+     res.status(500).send('Error');
    } else if (result) {
   console.log("Request for ID: " + req.params.id);
   console.log("Result: " + result + "\n");
   res.send(result);
    } else {
      console.log("Ticket not here");
-     res.send("Ticket not here");
+     res.status(404).send("Ticket not here");
    }
-    res.end();
   });
 });
 
@@ -87,40 +83,35 @@ app.get('/rest/ticket/:id', function(req, res) {
 
 // A POST request to insert a record by appending it as a string in a new line
 app.post('/rest/maketicket', function(req, res) {
-  const collection = client.db('clmdb').collection('ticket');
   const ticket = req.body;
   collection.insertOne(ticket, (err, result) => {
     if (err) {
       console.log('MongoDB error:', err);
-      res.send('Error ' + err);
+      res.status(500).send('Error');
     } else {
       console.log("Ticket inserted");
       res.send(ticket + "<p>in database");
     }
-    res.end();
   });
 });
 
 
 //A DELETE request to delete a record by ID from the database
 app.delete('/rest/ticket/:id', function(req, res) {
-  const collection = client.db('clmdb').collection('ticket');
   const id = req.params.id;
-  collection.deleteOne({ _id: id }, function (err, result) {
+  collection.deleteOne({ _id: ObjectId(id) }, function (err, result) {
       if (result.deletedCount === 0) {
         res.status(404).send('error');
       } else {
         console.log('Ticket with ID ${id} deleted');
         res.send('Ticket with ID ${id} deleted');
     }
-    res.end();
   });
 });
 
 
 //Update a ticket
 app.put('/rest/ticket/:id', function(req, res) {
-  const collection = client.db('clmbd').collection('ticket');
   const id = req.params.id;
   const updates = req.body;
   collection.updateOne({ _id: ObjectId(id) }, { $set: updates }, function(err, result) {
@@ -132,6 +123,5 @@ app.put('/rest/ticket/:id', function(req, res) {
           console.log('Ticket with ID ${id} updated');
           res.send('Ticket with ID ${id} updated');
       }
-      res.end();
     });
 });
