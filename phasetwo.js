4fa1ce17179af4ea73e184b06d7a6b2d2147a8ab
carreylipps <85637986+carreylipps@users.noreply.github.com>
2023-04-21 20:34:21 -0500

    Update phasetwo.js

--- phasetwo.js
+++ phasetwo.js
@@ -4,8 +4,7 @@ const express = require('express');
 const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
-const { MongoClient } = require('mongodb');
-const { ObjectId } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 app.listen(port, () => {
 console.log('Server started at port:' + port);
